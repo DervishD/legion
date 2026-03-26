547ab868560c4916f7ab9f6f5ee8c999b4fd3b29
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-03-26 19:54:11 +0100

    Fix missing staged content in commit df3efd0

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 """Configuration file for pytest."""
+import logging
 from typing import TYPE_CHECKING
 
 import pytest
 
+import legion
 from tests.helpers import LogPaths
 
 if TYPE_CHECKING:
@@ -21,3 +23,17 @@ def log_paths(tmp_path: Path) -> Generator[LogPaths]:
 
     main_output_path.unlink()
     full_output_path.unlink()
+
+
+@pytest.fixture
+def logger(log_paths: LogPaths) -> Generator[legion.Logger]:
+    """Set up and return a logger, configured using *log_paths*."""
+    logger_instance = legion.get_logger(__name__)
+
+    logger_instance.config(main_log_output=log_paths.main, full_log_output=log_paths.full)
+
+    yield logger_instance
+
+    logging.shutdown()
+
+
