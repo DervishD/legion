0904bcbf96ffa7922f068c41dd37ed498822d77f
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-04-13 21:28:06 +0200

    Add test units for timestamp()

---
+++ tests/test_timestamp.py
@@ -0,0 +1,29 @@
+"""Test units for `timestamp()` function."""
+from typing import TYPE_CHECKING
+
+from legion import timestamp
+from tests.helpers import CallableSpy
+
+if TYPE_CHECKING:
+    import pytest
+
+
+# pylint: disable-next=unused-variable
+def test_timestamp(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Test `timestamp()` function."""
+    mock_format = 'Mock timestamp template, will be ignored'
+    mock_timestring = '2084-07-07 06:54:54.9'  # TMA-1
+
+    def mock_strftime(_: str) -> str:
+        return mock_timestring
+
+    strftime_spy = CallableSpy(mock_strftime)
+
+    monkeypatch.setitem(timestamp.__globals__, 'strftime', strftime_spy)
+
+    timestamp()
+    timestamp(mock_format)
+
+    assert strftime_spy.called
+    assert strftime_spy.call_count == 2  # noqa: PLR2004
+    assert strftime_spy.calls == [(mock_timestring, ('%Y%m%d_%H%M%S',), {}),(mock_timestring, (mock_format,), {})]
