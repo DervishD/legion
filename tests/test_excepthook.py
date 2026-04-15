bc43ec618dd66d40663bfa40c57baea389efb9f7
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-04-15 16:16:37 +0200

    Rewrite tests for _format_traceback()
    The previous tests were too complex, required a weird setup, and did not
    cover neither all the code nor all the possible scenarios. Now they are
    better structured, cleaner, simpler and more readable.

--- tests/test_excepthook.py
+++ tests/test_excepthook.py
@@ -1,8 +1,6 @@
 """Test units for `excepthook()` function and helpers."""
 import errno
 from os import strerror
-from random import choice, randint, sample
-from string import ascii_lowercase
 import sys
 import traceback
 from traceback import FrameSummary, StackSummary
@@ -109,34 +107,140 @@ def test__format_exception_details(exception: Exception, expected: str) -> None:
     assert legion._format_exception_details(exception) == expected
 
 
+@pytest.mark.parametrize(('frames', 'expected'), [
+    pytest.param(
+        [],
+        '',
+        id='test__format_traceback_no_frames',
+    ),
+    pytest.param(
+        [FrameSummary('mock_file.py', 42, 'mock_func', line='mock_line_of_code')],
+        (
+            '⟶ mock_file.py\n'
+            '  42, mock_func: mock_line_of_code'
+        ),
+        id='test__format_traceback_single_frame',
+    ),
+    pytest.param(
+        [FrameSummary('mock_file.py', None, 'mock_func', line='mock_line_of_code')],
+        (
+            '⟶ mock_file.py\n'
+            '  1, mock_func: mock_line_of_code'
+        ),
+        id='test__format_traceback_none_lineno',
+    ),
+    pytest.param(
+        [FrameSummary('mock_file.py', 0, 'mock_func', line='mock_line_of_code')],
+        (
+            '⟶ mock_file.py\n'
+            '  1, mock_func: mock_line_of_code'
+        ),
+        id='test__format_traceback_zero_lineno',
+    ),
+    pytest.param(
+        [FrameSummary('mock_file.py', 42, 'mock_func')],
+        (
+            '⟶ mock_file.py\n'
+            '  42, mock_func'
+        ),
+        id='test__format_traceback_no_source',
+    ),
+    pytest.param(
+        [
+            FrameSummary('mock_file.py',  42, 'mock_func_1', line='mock_line_of_code_1'),
+            FrameSummary('mock_file.py',  77, 'mock_func_2', line='mock_line_of_code_2'),
+            FrameSummary('mock_file.py', 137, 'mock_func_3', line='mock_line_of_code_3'),
+        ],
+        (
+            '⟶ mock_file.py\n'
+            '  42, mock_func_1: mock_line_of_code_1\n'
+            '  77, mock_func_2: mock_line_of_code_2\n'
+            '  137, mock_func_3: mock_line_of_code_3'
+        ),
+        id='test__format_traceback_single_file',
+    ),
+    pytest.param(
+        [
+            FrameSummary('mock_file_1.py',  42, 'mock_func_1_1', line='mock_line_of_code_1_1'),
+            FrameSummary('mock_file_1.py',  77, 'mock_func_1_2', line='mock_line_of_code_1_2'),
+            FrameSummary('mock_file_1.py', 137, 'mock_func_1_3', line='mock_line_of_code_1_3'),
+            FrameSummary('mock_file_2.py',  42, 'mock_func_2_1', line='mock_line_of_code_2_1'),
+            FrameSummary('mock_file_2.py',  77, 'mock_func_2_2', line='mock_line_of_code_2_2'),
+            FrameSummary('mock_file_2.py', 137, 'mock_func_2_3', line='mock_line_of_code_2_3'),
+            FrameSummary('mock_file_3.py',  42, 'mock_func_3_1', line='mock_line_of_code_3_1'),
+            FrameSummary('mock_file_3.py',  77, 'mock_func_3_2', line='mock_line_of_code_3_2'),
+            FrameSummary('mock_file_3.py', 137, 'mock_func_3_3', line='mock_line_of_code_3_3'),
+        ],
+        (
+            '⟶ mock_file_1.py\n'
+            '  42, mock_func_1_1: mock_line_of_code_1_1\n'
+            '  77, mock_func_1_2: mock_line_of_code_1_2\n'
+            '  137, mock_func_1_3: mock_line_of_code_1_3\n'
+            '⟶ mock_file_2.py\n'
+            '  42, mock_func_2_1: mock_line_of_code_2_1\n'
+            '  77, mock_func_2_2: mock_line_of_code_2_2\n'
+            '  137, mock_func_2_3: mock_line_of_code_2_3\n'
+            '⟶ mock_file_3.py\n'
+            '  42, mock_func_3_1: mock_line_of_code_3_1\n'
+            '  77, mock_func_3_2: mock_line_of_code_3_2\n'
+            '  137, mock_func_3_3: mock_line_of_code_3_3'
+        ),
+        id='test__format_traceback_multiple_files',
+    ),
+    pytest.param(
+        [
+            FrameSummary('mock_file_1.py',  42, 'mock_func_1_1', line='mock_line_of_code_1_1'),
+            FrameSummary('mock_file_2.py',  42, 'mock_func_2_1', line='mock_line_of_code_2_1'),
+            FrameSummary('mock_file_1.py',  77, 'mock_func_1_2', line='mock_line_of_code_1_2'),
+        ],
+        (
+            '⟶ mock_file_1.py\n'
+            '  42, mock_func_1_1: mock_line_of_code_1_1\n'
+            '⟶ mock_file_2.py\n'
+            '  42, mock_func_2_1: mock_line_of_code_2_1\n'
+            '⟶ mock_file_1.py\n'
+            '  77, mock_func_1_2: mock_line_of_code_1_2'
+        ),
+        id='test__format_traceback_repeated_file',
+    ),
+])
 # pylint: disable-next=unused-variable
-def test_excepthook_format_traceback(monkeypatch: pytest.MonkeyPatch) -> None:
+def test__format_traceback(monkeypatch: pytest.MonkeyPatch, frames: list[FrameSummary], expected: str) -> None:
     """Test `_format_traceback()`."""
-    min_items = 3
-    max_items = 9
-    min_lineno = 1
-    max_lineno = 999
-
-    expected = ''
-    mock_frames: list[FrameSummary] = []
-
-    # ruff: disable[S311]
-    for file_index in sample(ascii_lowercase, randint(min_items, max_items)):
-        filename = f'source_file_{file_index}.py'
-        expected += f'⟶ {filename}\n'
-        for _j in range(randint(min_items, max_items)):
-            location = (randint(min_lineno, max_lineno), f'func_{choice(ascii_lowercase)}')
-            codeline = f'source_code_{choice(ascii_lowercase)}'
-            mock_frames.append(FrameSummary(filename, *location, line=codeline))
-            expected += '  {}, {}: {}\n'.format(*location, codeline)  # pylint: disable=consider-using-f-string
-    # ruff: enable[S311]
-
     def mock_extract_tb (_: TracebackType) -> StackSummary:
-        return StackSummary.from_list(mock_frames)
-
+        return StackSummary.from_list(frames)
     monkeypatch.setattr(traceback, 'extract_tb', mock_extract_tb)
+    assert legion._format_traceback(None) == expected
 
-    assert legion._format_traceback(None) == expected.removesuffix('\n')
+
+@pytest.mark.parametrize(('lines', 'expected'), [
+    pytest.param(
+        [],
+        'fallback',
+        id='test__format_trackeback_linecache_empty',
+    ),
+    pytest.param(
+        [
+            'source_code_part1_',
+            '_part2_split_',
+            '_part3_end',
+        ],
+        'source_code_part1__part2_split__part3_end',
+        id='test__format_trackeback_linecache_baseline',
+    ),
+
+])
+# pylint: disable-next=unused-variable
+def test__format_traceback_linecache(monkeypatch: pytest.MonkeyPatch, lines: list[str], expected: str) -> None:
+    """Test `_format_traceback()` `linecache` handling and fallback."""
+    def mock_extract_tb (_: TracebackType) -> StackSummary:
+        return StackSummary.from_list([FrameSummary('mock_file.py', 1, 'mock_func', end_lineno=3, line='fallback')])
+    monkeypatch.setattr(traceback, 'extract_tb', mock_extract_tb)
+    def mock_getlines(filename: str, *_: object) -> list[str]:
+        assert filename == 'mock_file.py'
+        return lines
+    monkeypatch.setattr('linecache.getlines', mock_getlines)
+    assert legion._format_traceback(None) == f'⟶ mock_file.py\n  1, mock_func: {expected}'
 
 
 @pytest.mark.parametrize(('has_args', 'has_traceback'), [
