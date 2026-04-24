2882a407781d132bd714b3f43189c255253f9585
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-04-24 23:19:58 +0200

    Improve metadata generation and update
    Since the about.py file was not in the repository, it was impossible to
    install the package except in editable mode. This was partially intended
    but a more flexible approach was needed.
    
    So, the about.py file is now version controlled, and updated when needed
    using the appropriate git hooks.

---
+++ src/legion/about.py
@@ -0,0 +1,7 @@
+"""Auto generated, do not edit."""
+# 2026-04-24 23:19:59
+
+# pylint: disable=unused-variable
+PROGRAM_NAME = 'legion'
+VERSION = '2.1.0.post151+development.ba47eb9.dirty'
+RELEASE = '2.1.0.post0'
