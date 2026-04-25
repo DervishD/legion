7ea28e3be08ae9d1578617675b7ebebc7fd51e2e
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-04-25 17:37:17 +0200

    Do not update editable install in git hooks
    It is too slow and expensive, and it does not make sense to do it after
    EACH commit.

--- src/legion/about.py
+++ src/legion/about.py
@@ -1,7 +1,7 @@
 """Auto generated, do not edit."""
-# 2026-04-25 17:36:04
+# 2026-04-25 17:37:18
 
 # pylint: disable=unused-variable
 PROGRAM_NAME = 'legion'
-VERSION = '2.1.0.post155+development.5c4a377.dirty'
+VERSION = '2.1.0.post156+development.3b50d19.dirty'
 RELEASE = '2.1.0.post0'
