85c4a399510b16ed0e1d46b7b07157b8dae1ba58
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-04-29 21:37:35 +0200

    Simplify __main__.py

--- src/legion/__main__.py
+++ src/legion/__main__.py
@@ -1,32 +1,20 @@
 """."""
-import contextlib
+from pathlib import Path
 import sys
 
-from legion import docs, ensure_utf8_output, excepthook, git_repository_root, load_pyproject
-
+from . import docs, ensure_utf8_output, excepthook, get_project_metadata
 
 @ensure_utf8_output
 def main() -> None:
     """."""
     sys.excepthook = excepthook
 
-    if (project_root := git_repository_root()) is None:
-        msg = f'{project_root=!r}'
+    if (metadata := get_project_metadata()) is None:
+        msg = f'{metadata = !r}'
         raise RuntimeError(msg)
 
-    if (project_metadata := load_pyproject()) is None:
-        msg = f'{project_metadata=!r}'
-        raise RuntimeError(msg)
-
-    readme_file = None
-    with contextlib.suppress(KeyError):
-        readme_file = project_metadata['project']['readme']
-
-    if readme_file is None:
-        msg = f'{readme_file=!r}'
-        raise RuntimeError(msg)
-
-    (project_root / readme_file).write_text(docs(), encoding='utf-8', newline='\n')
+    readme_path = Path(metadata['proaject_root']).resolve() / metadata['project']['readme']
+    readme_path.write_text(docs(), encoding='utf-8', newline='\n')
 
 
 sys.exit(main())
