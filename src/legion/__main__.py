e809a707feb4f7951aa6813035fe3d669cf4c828
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-04-30 15:49:52 +0200

    Fix two typos in __main__.py

--- src/legion/__main__.py
+++ src/legion/__main__.py
@@ -4,6 +4,7 @@ import sys
 
 from . import docs, ensure_utf8_output, excepthook, get_project_metadata
 
+
 @ensure_utf8_output
 def main() -> None:
     """."""
@@ -13,7 +14,7 @@ def main() -> None:
         msg = f'{metadata = !r}'
         raise RuntimeError(msg)
 
-    readme_path = Path(metadata['proaject_root']).resolve() / metadata['project']['readme']
+    readme_path = Path(metadata['project_root']).resolve() / metadata['project']['readme']
     readme_path.write_text(docs(), encoding='utf-8', newline='\n')
 
 
